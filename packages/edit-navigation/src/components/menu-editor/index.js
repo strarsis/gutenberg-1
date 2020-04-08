d32c4fc14b1958b44fa0e6fc4dbacb6eb15eb073
Andrei Draganescu <andrei.draganescu@automattic.com>
2020-04-08 17:05:16 +0300

    adds delete with menus as state
    Given deleting entities is a complex endeavor, that will eventually allow to undo deletes before saving, this approach advances the navigation screen and allows for user testing.

--- packages/edit-navigation/src/components/menu-editor/index.js
+++ packages/edit-navigation/src/components/menu-editor/index.js
@@ -1,6 +1,12 @@
 /**
+ * External dependencies
+ */
+import { find } from 'lodash';
+
+/**
  * WordPress dependencies
  */
+import apiFetch from '@wordpress/api-fetch';
 import {
 	BlockEditorKeyboardShortcuts,
 	BlockEditorProvider,
@@ -12,6 +18,7 @@ import {
 import { __ } from '@wordpress/i18n';
 import { Panel, PanelBody, Button } from '@wordpress/components';
 import { useViewportMatch } from '@wordpress/compose';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -19,9 +26,40 @@ import { useViewportMatch } from '@wordpress/compose';
 import useNavigationBlocks from './use-navigation-blocks';
 import MenuEditorShortcuts from './shortcuts';
 
-export default function MenuEditor( { menuId, blockEditorSettings } ) {
+export default function MenuEditor( {
+	menuId,
+	blockEditorSettings,
+	onDelete,
+} ) {
+	const kind = 'root';
+	const name = 'menu';
+
 	const [ blocks, setBlocks, saveBlocks ] = useNavigationBlocks( menuId );
 	const isLargeViewport = useViewportMatch( 'medium' );
+	const entities = useSelect( ( select ) =>
+		select( 'core' ).getEntitiesByKind( kind )
+	);
+
+	const entity = find( entities, { kind, name } );
+
+	const deleteMenu = async ( recordId ) => {
+		const path = `${ entity.baseURL + '/' + recordId + '?force=true' }`;
+		const deletedRecord = await apiFetch( {
+			path,
+			method: 'DELETE',
+		} );
+		return deletedRecord.previous;
+	};
+
+	const askToDelete = async () => {
+		if (
+			// eslint-disable-next-line no-alert
+			window.confirm( __( 'Are you sure you want to delete this menu?' ) )
+		) {
+			const deletedMenu = await deleteMenu( menuId );
+			onDelete( deletedMenu );
+		}
+	};
 
 	return (
 		<div className="edit-navigation-menu-editor">
@@ -55,15 +93,16 @@ export default function MenuEditor( { menuId, blockEditorSettings } ) {
 						) }
 					</PanelBody>
 				</Panel>
-				<Panel
-					header={
-						<Button isPrimary onClick={ saveBlocks }>
-							{ __( 'Save navigation' ) }
-						</Button>
-					}
-					className="edit-navigation-menu-editor__panel"
-				>
+				<Panel className="edit-navigation-menu-editor__panel">
 					<PanelBody title={ __( 'Navigation menu' ) }>
+						<div className="components-panel__header-actions">
+							<Button isPrimary onClick={ saveBlocks }>
+								{ __( 'Save navigation' ) }
+							</Button>
+							<Button isPrimary onClick={ askToDelete }>
+								{ __( 'Delete navigation' ) }
+							</Button>
+						</div>
 						<WritingFlow>
 							<ObserveTyping>
 								<BlockList />
