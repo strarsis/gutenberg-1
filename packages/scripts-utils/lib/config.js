d8a1a36b9c0b6f780d83b4e1bc51cc4716205ae3
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-04-09 15:08:14 +0200

    Scripts utils: Add support for all file extensions for config lookup

--- packages/scripts-utils/lib/config.js
+++ packages/scripts-utils/lib/config.js
@@ -5,30 +5,44 @@ const { hasArgInCLI } = require( './cli' );
 const { hasProjectFile } = require( './file' );
 const { hasPackageProp } = require( './package' );
 
-// See https://babeljs.io/docs/en/config-files#configuration-file-types
+/**
+ * @see https://babeljs.io/docs/en/config-files#configuration-file-types
+ */
 const hasBabelConfig = () =>
+	hasProjectFile( '.babelrc' ) ||
+	hasProjectFile( '.babelrc.cjs' ) ||
 	hasProjectFile( '.babelrc.js' ) ||
 	hasProjectFile( '.babelrc.json' ) ||
+	hasProjectFile( '.babelrc.mjs' ) ||
+	hasProjectFile( 'babel.config.cjs' ) ||
 	hasProjectFile( 'babel.config.js' ) ||
 	hasProjectFile( 'babel.config.json' ) ||
-	hasProjectFile( '.babelrc' ) ||
+	hasProjectFile( 'babel.config.mjs' ) ||
 	hasPackageProp( 'babel' );
 
+/**
+ * @see https://jestjs.io/docs/en/configuration
+ */
 const hasJestConfig = () =>
 	hasArgInCLI( '-c' ) ||
 	hasArgInCLI( '--config' ) ||
+	hasProjectFile( 'jest.config.cjs' ) ||
 	hasProjectFile( 'jest.config.js' ) ||
 	hasProjectFile( 'jest.config.json' ) ||
+	hasProjectFile( 'jest.config.mjs' ) ||
 	hasPackageProp( 'jest' );
 
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ */
 const hasPrettierConfig = () =>
+	hasProjectFile( '.prettierrc' ) ||
 	hasProjectFile( '.prettierrc.js' ) ||
 	hasProjectFile( '.prettierrc.json' ) ||
 	hasProjectFile( '.prettierrc.toml' ) ||
 	hasProjectFile( '.prettierrc.yaml' ) ||
 	hasProjectFile( '.prettierrc.yml' ) ||
 	hasProjectFile( 'prettier.config.js' ) ||
-	hasProjectFile( '.prettierrc' ) ||
 	hasPackageProp( 'prettier' );
 
 module.exports = {
