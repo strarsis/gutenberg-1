varnasicoverage/gutenberg

Memoize useSelect callback function
Co-Authored-By: Zebulan Stanphill <zebulanstanphill@protonmail.com>

packages/edit-navigation/src/components/menu-editor/navigation-structure-panel.js
@@ -10,8 +10,9 @@ import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 export default function NavigationStructurePanel( { blocks } ) {
-	const selectedBlockClientIds = useSelect( ( select ) =>
-		select( 'core/block-editor' ).getSelectedBlockClientIds()
+	const blockClientIds = useSelect(
+		( select ) => select( 'core/block-editor' ).getSelectedBlockClientIds(),
+		[]
 	);
 
 	const { selectBlock } = useDispatch( 'core/block-editor' );

strarsis/gutenberg-1

packages/edit-navigation/src/components/menu-editor/navigation-structure-panel.js
@@ -10,11 +10,12 @@ import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 export default function NavigationStructurePanel( { blocks, initialOpen } ) {
-	const selectedBlockClientIds = useSelect( ( select ) =>
-		select( 'core/block-editor' ).getSelectedBlockClientIds()
+	const selectedBlockClientIds = useSelect(
+		( select ) => select( 'core/block-editor' ).getSelectedBlockClientIds(),
+		[]
 	);
-
 	const { selectBlock } = useDispatch( 'core/block-editor' );
+
 	return (
 		<Panel className="edit-navigation-menu-editor__navigation-structure-panel">
 			<PanelBody